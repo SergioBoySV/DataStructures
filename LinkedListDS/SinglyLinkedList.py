3b0410fa121aa2982943379098ad645b5c7059fc
SergioBoySV <darknight_777@hotmail.com>
2023-11-05 01:14:36 -0500

    Meant to commit this file

--- LinkedListDS/SinglyLinkedList.py
+++ LinkedListDS/SinglyLinkedList.py
@@ -2,7 +2,13 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
-        self.index = None  # Tracks index of the node. May make insertAtIndex possible, but maybe not
+
+
+def createNode(data):
+    createdNode = Node(data)
+    createdNode.next = None
+    createdNode.index = None
+    return createdNode
 
 
 class LinkedList:
@@ -17,12 +23,23 @@ class LinkedList:
             yield node
             node = node.next
 
-    def insertAtBeginning(self, newNode: Node):
-        newNode.next = self.head
-        self.head = newNode  # New head is newNode
-        self.size += 1
+    def insertAtBeginning(self, newNode):
+        if not isinstance(newNode, Node):
+            newNode = createNode(newNode)
 
-    def insertAtEnd(self, newNode: Node):
+        newNode.next = self.head
+        self.head = newNode
+        self.size += 1
+        return
+
+    def insertAtEnd(self, newNode, tailNode=None):
+        if tailNode is not None:
+            tailNode.next = newNode
+            # self.tail = newNode
+            return
+
+        if not isinstance(newNode, Node):
+            newNode = createNode(newNode)
         if self.isEmpty():
             self.head = newNode
             self.size += 1
@@ -36,49 +53,73 @@ class LinkedList:
         #     tmpHead = tmpHead.next
         # tmpHead.next = node
 
-    def insertAfter(self, targetNodeData, newNode: Node):
+    def insertAfter(self, targetNode, newNode):
         if self.isEmpty():
             raise Exception('Linked List is empty')
-        # if self.tail.data == targetNodeData:  # If trying to after before tail, call insertAtEnd
-        #     return self.insertAtEnd(newNode)
-        for node in self:
-            if node.data == targetNodeData:
-                newNode.next = node.next
-                node.next = newNode
-                self.size += 1
-                return
-        raise Exception(f'Node with data: {targetNodeData} does not exist!')
+        if not isinstance(newNode, Node):
+            newNode = createNode(newNode)
 
-    def insertBefore(self, targetNodeData, newNode: Node):
+        if isinstance(targetNode, Node):
+            # if self.tail.data == targetNode.data:
+            #     self.insertAtEnd(newNode, tailNode=self.tail)
+            #     return
+            newNode.next = targetNode.next
+            targetNode.next = newNode
+            return
+
+        if not isinstance(targetNode, Node):
+            # if self.tail.data == targetNode:  # If trying to insert after tail, call insertAtEnd
+            #     return self.insertAtEnd(newNode)
+            for node in self:
+                if node.data == targetNode:
+                    newNode.next = node.next
+                    node.next = newNode
+                    self.size += 1
+                    return
+
+        raise Exception(f'Node with data: {targetNode} does not exist!')
+
+    def insertBefore(self, targetNode, newNode):
         if self.isEmpty():
             raise Exception('Linked List is empty')
-        if self.head.data == targetNodeData:  # If trying to insert before head, call insertAtBeginning
+        if not isinstance(newNode, Node):
+            newNode = createNode(newNode)
+        if not isinstance(targetNode, Node):
+            targetNode = createNode(targetNode)
+
+        if self.head.data == targetNode.data:
             return self.insertAtBeginning(newNode)
+
         previousNode = self.head
         for node in self:
-            if node.data == targetNodeData:
+            if node.data == targetNode.data:
                 previousNode.next = newNode
                 newNode.next = node
                 self.size += 1
                 return
             previousNode = node
-        raise Exception(f'Node with data: {targetNodeData} does not exist!')
 
-    def removeNode(self, targetNodeData):
+        raise Exception(f'Node with data: {targetNode} does not exist!')
+
+    def removeNode(self, targetNode):
         if self.isEmpty():
             raise Exception('Linked List is empty')
-        if self.head.data == targetNodeData:
-            self.head = self.head.next
-            self.size -= 1
-            return
+        if not isinstance(targetNode, Node):
+            targetNode = createNode(targetNode)
+
+        if self.head.data == targetNode.data:
+            return self.removeHead()
+        # if self.tail.data == targetNode.data:
+        #     return self.removeTail()
         previousNode = self.head
         for node in self:
-            if node.data == targetNodeData:
+            if node.data == targetNode.data:
                 previousNode.next = node.next
                 self.size -= 1
                 return
             previousNode = node
-        raise Exception(f'Node with data: {targetNodeData} does not exist!')
+
+        raise Exception(f'Node with data: {targetNode.data} does not exist!')
 
     def removeHead(self):
         if self.isEmpty():
@@ -86,9 +127,14 @@ class LinkedList:
         self.head = self.head.next
         self.size -= 1
 
-    def removeTail(self):
+    def removeTail(self):  # Needs work
+        if self.isEmpty():
+            raise Exception('Linked List is empty')
+        del self.tail
+        for node in self:  # iterate until we get to the last element
+            pass
+        self.tail = node
         self.size -= 1
-        pass
 
     def getSize(self) -> int:
         return self.size
@@ -102,6 +148,16 @@ class LinkedList:
     def removeAtIndex(self, index: int):
         pass
 
+    def search(self, searchValue) -> Node:
+        if self.isEmpty():
+            raise Exception('Linked List is empty')
+        if not isinstance(searchValue, Node):
+            searchValue = createNode(searchValue)
+        for node in self:
+            if node.data == searchValue.data:
+                return node
+        raise Exception(f'Element {searchValue.data} does not exist in Linked List!')
+
     def printLinkedList(self) -> None:
         for node in self:
             print(f'{node.data} --> ', end='')
@@ -110,7 +166,13 @@ class LinkedList:
 
 # ----- Testing Linked List -----
 myLL = LinkedList()
-print(myLL.getSize())
+# array = [1, 10, 2, 20, 3, 30, 50]
+#
+# for i in array:
+#     myLL.insertAtEnd(i)
+#
+# myLL.printLinkedList()
+
 Node1 = Node(10)
 Node2 = Node(20)
 Node3 = Node(30)
@@ -120,19 +182,22 @@ Node6 = Node(110)
 Node7 = Node(-1)
 
 myLL.insertAtBeginning(Node1)
-myLL.insertAtBeginning(Node2)
-myLL.insertAtBeginning(Node3)
-myLL.insertAtBeginning(Node4)
-
-myLL.insertAtEnd(Node5)
-myLL.insertAfter(10, Node6)
-myLL.insertBefore(20, Node7)
-
-myLL.removeNode(-1)
-myLL.removeHead()
-
+myLL.insertAtEnd(Node2)
+myLL.insertAtEnd(Node3)
+myLL.insertAfter(Node2, Node7)
+myLL.insertBefore(Node3, Node6)
+myLL.removeNode(Node7)
+myLL.insertBefore(Node1, Node7)
+myLL.insertAfter(Node3, Node5)
 myLL.printLinkedList()
-print(myLL.getSize())
+
+# myLL.insertAtBeginning(1)
+# myLL.insertAtEnd(2)
+# myLL.insertAtEnd(3)
+# myLL.insertAfter(2, 2.5)
+# myLL.insertBefore(2, 1.5)
+# myLL.removeNode(2)
+# myLL.printLinkedList()
 
 
 '''
